22f700fe9441ff956b6889bc9d9c8638cd53f468
m_vokhm <jgault55@yahoo.com>
2021-06-05 20:12:56 +0500

    Prepare environment to play with division

---
+++ src/main/java/com/mvohm/quadruple/research/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Various experimental things to find a way to accelerate the division
+ *
+ * Started 21.06.05 18:44:37
+ *
+ * @author M.Vokhmentev
+ *
+ */
+package com.mvohm.quadruple.research;
